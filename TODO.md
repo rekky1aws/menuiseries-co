1e6b849bb97ba0dfcd71209882b55655399937c5
rekky1aws <killikomfr@gmail.com>
2023-09-21 00:21:30 +0200

    Img gallery and JS Init

--- TODO.md
+++ TODO.md
@@ -2,6 +2,8 @@
 
 ## A faire
  + Ajouter un carroussel d'images pour demo
+ 	+ ajouter du CSS pour améliorer le style du Carousel
+ 	+ ajouter le code JS pour récupérer les 5 dernières images postés sur Instagram
  + Mentions légales
 
 ## Fait
---
+++ script/app.js
@@ -0,0 +1,19 @@
+function getInstaToken ()
+{
+	// recupérer token
+}
+
+function getInsta5Posts (token, username="hulot.christophe") {
+	// récuperer les images des 5 derniers posts Insta
+}
+
+
+/*
+https://api.instagram.com/oauth/authorize
+  ?client_id=977283373501487977283373501487
+  &redirect_uri=https://localhost/
+  &scope=user_profile,user_media
+  &response_type=code
+
+https://api.instagram.com/oauth/authorize?client_id=977283373501487977283373501487&redirect_uri=https://localhost/&scope=user_profile,user_media&response_type=code
+*/
